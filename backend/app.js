c1900f55b1d287fdb65ac173e489e41d2b858913
Amiya Ranjan <amiya26ranjan@gmail.com>
2024-08-03 23:34:47 +0530

    www enabled

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const { isAuthenticated, isSeller, isAdmin } = require("./middleware/auth");
 
 // CORS configuration
 const corsOptions = {
-  origin: ['https://vaymp.com'], // Replace with your frontend origin
+  origin: ['https://www.vaymp.com','https://vaymp.com'], // Replace with your frontend origin
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true, // Only if you need cookies or authentication
