57bd92c069bbe94999a0700dcbbf25e89f93e776
Amiya Ranjan <amiya26ranjan@gmail.com>
2024-10-23 02:37:20 +0530

    number changed

--- frontend/src/components/Admin/AdminDashboardMain.jsx
+++ frontend/src/components/Admin/AdminDashboardMain.jsx
@@ -120,7 +120,7 @@ const adminEarning = data && data.reduce((acc, item) => {
                 Total Earning
               </h3>
             </div>
-            <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">Rs 1436</h5>
+            <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">Rs 1435.87</h5>
             {/* <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">Rs{adminBalance}</h5> */}
 
           </div>
@@ -138,7 +138,7 @@ const adminEarning = data && data.reduce((acc, item) => {
               </h3>
             </div>
             {/* <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">Rs{adminTotalRevenue}</h5> */}
-            <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">Rs 19145</h5>
+            <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">Rs 19145.00</h5>
 
           </div>
           <div className="w-full mb-4 800px:w-[30%] min-h-[20vh] bg-white shadow rounded px-2 py-5">
